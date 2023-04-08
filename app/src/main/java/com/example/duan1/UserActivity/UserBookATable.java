daf8e255e7baed96b8138fe93b730e45437b6916
n3kofpt <anhnttps24703@fpt.edu.vn>
2023-04-08 15:39:27 +0700

    UserBookATable

---
+++ app/src/main/java/com/example/duan1/UserActivity/UserBookATable.java
@@ -0,0 +1,16 @@
+package com.example.duan1.UserActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.duan1.R;
+
+public class UserBookATable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_book_atable);
+    }
+}
