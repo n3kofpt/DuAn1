b3e1653e100227aacd0d1d6c440a1da5de8db2a8
LILLINH <tunglinhwork@gmail.com>
2023-04-12 01:04:26 +0700

    main

--- app/src/main/java/com/example/duan1/UserActivity/UserMainActivity.java
+++ app/src/main/java/com/example/duan1/UserActivity/UserMainActivity.java
@@ -2,7 +2,9 @@ package com.example.duan1.UserActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.duan1.R;
 
@@ -13,4 +15,13 @@ public class UserMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_main);
     }
+    public void onlickMain(View view) {
+        Intent intent = new Intent(UserMainActivity.this, UserBookATable.class);
+        startActivity(intent);
+    }
+    public void onlickMain1(View view) {
+        Intent intent = new Intent(UserMainActivity.this, lichhen.class);
+        startActivity(intent);
+    }
+
 }
