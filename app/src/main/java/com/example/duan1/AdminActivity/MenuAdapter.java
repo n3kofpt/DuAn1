f31afb9cacb423ab076bb38a803d90c404bf5146
n3kofpt <anhnttps24703@fpt.edu.vn>
2023-04-12 21:22:50 +0700

    Menu item on long click

--- app/src/main/java/com/example/duan1/AdminActivity/MenuAdapter.java
+++ app/src/main/java/com/example/duan1/AdminActivity/MenuAdapter.java
@@ -1,17 +1,28 @@
 package com.example.duan1.AdminActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.duan1.R;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.List;
 
 public class MenuAdapter extends RecyclerView.Adapter<MenuViewHolder> {
     private List<Menu> menuList;
+    String[] options = new String[]{"Sửa", "Xóa"};
+
+    public interface ItemClickListener {
+        void onClick(View view, int position,boolean isLongClick);
+    }
 
     public MenuAdapter(List<Menu> menuList) {
         this.menuList = menuList;
@@ -24,9 +35,10 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuViewHolder> {
 
     @Override
     public MenuViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // lấy layout của menu item
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.menu_item, parent, false);
-        return new MenuViewHolder(view);
+        return new MenuViewHolder(view); // Trả về menu item view
     }
 
     @Override
@@ -36,6 +48,52 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuViewHolder> {
         holder.imageView.setImageResource(menu.getImageResourceId());
         holder.foodNameTextView.setText(menu.getFoodName());
         holder.priceTextView.setText(menu.getPrice());
+
+        holder.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void onClick(View view, int position, boolean isLongClick) {
+                if(isLongClick){
+                    // xử lý long click
+                    Log.d(">>>>>>>>>>>>>>>>", "onLongClick: " + menuList.get(position));
+                    AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                    builder.setItems(options, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
+                                case 0:
+                                    // Xử lý khi người dùng chọn "Sửa"
+
+                                    break;
+                                case 1:
+                                    // Xử lý khi người dùng chọn "Xóa"
+                                    String foodName = menuList.get(position).getFoodName();
+                                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                    db.collection("menu")
+                                            .whereEqualTo("foodName", foodName)
+                                            .get()
+                                            .addOnSuccessListener(queryDocumentSnapshots -> {
+                                                for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                                                    document.getReference().delete();
+                                                }
+                                                Toast.makeText(view.getContext(), "Xóa món thành công", Toast.LENGTH_SHORT).show();
+                                            })
+                                            .addOnFailureListener(e -> {
+                                                Log.d("MenuAdapter", "Lỗi khi xóa món: ", e);
+                                                Toast.makeText(view.getContext(), "Lỗi khi xóa món", Toast.LENGTH_SHORT).show();
+                                            });
+                                    break;
+                            }
+                        }
+                    });
+                    builder.show();
+                }
+                else {
+                    // xử lý 1 click
+                    Log.d(">>>>>>>>>>>>>>>>", "onClick: " + menuList.get(position));
+                }
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/duan1/AdminActivity/MenuViewHolder.java
+++ app/src/main/java/com/example/duan1/AdminActivity/MenuViewHolder.java
@@ -1,5 +1,6 @@
 package com.example.duan1.AdminActivity;
 
+import android.app.Application;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,16 +9,38 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.duan1.R;
 
-public class MenuViewHolder extends RecyclerView.ViewHolder {
+public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
     public ImageView imageView;
     public TextView foodNameTextView;
     public TextView priceTextView;
+
+    private MenuAdapter.ItemClickListener itemClickListener;  // Khai báo interface
 
     public MenuViewHolder(View itemView) {
         super(itemView);
         imageView = itemView.findViewById(R.id.menu_item_image);
         foodNameTextView = itemView.findViewById(R.id.menu_item_food_name);
         priceTextView = itemView.findViewById(R.id.menu_item_price);
+
+        itemView.setOnClickListener(this); // Mấu chốt ở đây , set sự kiên onClick cho View
+        itemView.setOnLongClickListener(this); // Mấu chốt ở đây , set sự kiên onLongClick cho View
+    }
+
+    //Tạo setter cho biến itemClickListenenr
+    public void setItemClickListener(MenuAdapter.ItemClickListener itemClickListener)
+    {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemClickListener.onClick(v,getAdapterPosition(),false);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        itemClickListener.onClick(v,getAdapterPosition(),true);
+        return true;
     }
 }
 
