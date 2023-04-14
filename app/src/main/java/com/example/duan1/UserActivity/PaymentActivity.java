f89101dd25df7672b0425c76db999a6182ff311e
SangNguyenSVN <nguyenvusang1108@gmail.com>
2023-04-15 01:23:13 +0700

    Merge remote-tracking branch 'origin/main'

--- app/src/main/java/com/example/duan1/UserActivity/PaymentActivity.java
+++ app/src/main/java/com/example/duan1/UserActivity/PaymentActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.duan1.R;
@@ -18,8 +20,15 @@ import com.example.duan1.adapter.PlaceYourOrderAdapter;
         TextView buttonPlaceYourOrder;
         RecyclerView cartItemsRecyclerView;
 
+        EditText edt_PayMoney;
+
         PlaceYourOrderAdapter placeYourOrderAdapter;
 
+        Reservation reservation;
+
+
+
+        @SuppressLint("MissingInflatedId")
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -36,32 +45,20 @@ import com.example.duan1.adapter.PlaceYourOrderAdapter;
             buttonPlaceYourOrder = findViewById(R.id.buttonPlaceYourOrder);
 
             cartItemsRecyclerView = findViewById(R.id.cartItemsRecyclerView);
-//            initRecyclerView(reservation);
+
+            edt_PayMoney = findViewById(R.id.edt_PayMoney);
+
+
+            calculateTotalAmount(reservation);
             buttonPlaceYourOrder.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent i = new Intent(PaymentActivity.this, OrderSucceessActivity.class);
-                    startActivity(i);
-                    isChecked(status);
+                    isChecked();
                 }
             });
 
-            // lấy dữ liệu từ Bundle
-            Intent intent = getIntent();
-            Bundle bundle = intent.getExtras();
-
-            int tablePosition = bundle.getInt("tablePosition");
-            String name = bundle.getString("name");
-            String phoneNumber = bundle.getString("phoneNumber");
-            String address = bundle.getString("address");
-            int hour = bundle.getInt("hour");
-            int minute = bundle.getInt("minute");
-            int day = bundle.getInt("day");
-            int month = bundle.getInt("month");
-            int year = bundle.getInt("year");
 
 
-//            Reservation reservation = new Reservation(tablePosition, name, phoneNumber, address, status, hour, minute, day, month, year, menu);
         }
     private void calculateTotalAmount(Reservation reservation) {
         String subTotalAmount = 0+"";
@@ -72,23 +69,28 @@ import com.example.duan1.adapter.PlaceYourOrderAdapter;
 
         tvSubtotalAmount.setText("$"+String.format("%.2f", subTotalAmount));
         tvTotalAmount.setText("$"+String.format("%.2f", subTotalAmount));
+
     }
     private void initRecyclerView(Reservation reservation) {
         cartItemsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         placeYourOrderAdapter = new PlaceYourOrderAdapter(reservation.getMenus());
         cartItemsRecyclerView.setAdapter(placeYourOrderAdapter);
     }
-
-    public void isChecked(boolean isCheck){
-        if(isCheck = true){
-            status = true;
-
-            Intent i = new Intent(PaymentActivity.this, OrderSucceessActivity.class);
-            startActivity(i);
-
-        }else {
-            status = false;
-
+    public void isChecked(){
+            Double d = Double.valueOf(edt_PayMoney.getText().toString());
+            String subTotalAmount = 0+"";
+            // lay tong gia tien
+            for(Menu m : reservation.getMenus()) {
+                subTotalAmount += m.getPrice();
+            }
+            Double subT = Double.valueOf(subTotalAmount);
+            if(edt_PayMoney.length()==0){
+                edt_PayMoney.requestFocus();
+                edt_PayMoney.setError("Không được bỏ trống");
+            }else if(d >= (subT/10)) {
+                // truyền dữ liệu
+                Intent i = new Intent(PaymentActivity.this, OrderSucceessActivity.class);
+                startActivity(i);
+            }
         }
     }
-}
